77f32234c2cca611f241c73e2b1fc05a0af28f6a
Samuel Monson <smonson@redhat.com>
2024-10-14 14:47:09 -0400

    Refactor cli and add list tasks option

--- perf_llm_eval/eval_bench.py
+++ perf_llm_eval/eval_bench.py
@@ -31,7 +31,7 @@ def exec_lm_eval(tasks, model, endpoint, **kwargs):
     tm = TaskManager(
         include_path=kwargs['tasks_path'],
         include_defaults=False,
-        verbosity=logging.getLevelName(kwargs['loglevel'])
+        verbosity=logging.getLevelName(logger.level)
     )
 
     logger.info("Running lm-eval")
@@ -56,16 +56,19 @@ def exec_lm_eval(tasks, model, endpoint, **kwargs):
         if results.get('groups'):
             print(make_table(results, "groups"))
 
-def eval_cli():
-    parser = argparse.ArgumentParser(
-                    description='Helper script for running PSAP relevent LLM benchmarks.',
-                    epilog='',
-                    formatter_class=argparse.ArgumentDefaultsHelpFormatter
+
+def list_tasks(tasks_path: str):
+    from lm_eval.tasks import TaskManager  # type: ignore
+    tm = TaskManager(
+        include_path=tasks_path,
+        include_defaults=False,
+        verbosity=logging.getLevelName(logger.level)
     )
 
-    local_dir = os.path.dirname(__file__)
-    work_dir = os.getcwd()
+    print(tm.list_all_tasks())
 
+
+def setup_parser(local_dir: str, work_dir: str):
     def dir_path(path: str) -> str:
         ''' Typecheck for directory '''
         if os.path.isdir(path):
@@ -73,28 +76,14 @@ def eval_cli():
         else:
             raise NotADirectoryError(path)
 
-    parser.add_argument('--endpoint', '-H', required=True,
-                        default='http://127.0.0.1:8000/v1/completions',
-                        help='OpenAI API-compatible endpoint')
-    parser.add_argument('--model', '-m', required=True,
-                        help="name of the model under test")
-    parser.add_argument('--tasks', '-t', required=True,
-                        help="comma separated list of tasks")
-    parser.add_argument('--datasets', '-d', type=dir_path, required=True,
-                        default=f"{work_dir}/datasets",
-                        help="path to dataset storage")
-    parser.add_argument('--batch_size', '-b', default=64, type=int,
-                        help="per-request batch size")
-    parser.add_argument('--catalog_path', type=dir_path,
+    parser_base = argparse.ArgumentParser(add_help=False)
+    parser_base.add_argument('--catalog_path', type=dir_path,
                         default=f"{local_dir}/lm_eval/catalog",
                         help="unitxt catalog directory")
-    parser.add_argument('--tasks_path', type=dir_path,
+    parser_base.add_argument('--tasks_path', type=dir_path,
                         default=f"{local_dir}/lm_eval/tasks",
                         help="lm-eval tasks directory")
-    parser.add_argument('--output', '-o', type=argparse.FileType('w'),
-                        default=f"{work_dir}/output.json",
-                        help="results output file")
-    log_group = parser.add_mutually_exclusive_group()
+    log_group = parser_base.add_mutually_exclusive_group()
     log_group.add_argument('--verbose', '-v', default=logging.INFO,
                            action="store_const", dest="loglevel", const=logging.DEBUG,
                            help="set loglevel to DEBUG")
@@ -102,6 +91,51 @@ def eval_cli():
                            action="store_const", dest="loglevel", const=logging.ERROR,
                            help="set loglevel to ERROR")
 
+
+    parser = argparse.ArgumentParser(
+        description='Helper script for running PSAP relevent LLM benchmarks.',
+        epilog='',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        parents=[parser_base]
+    )
+    subparsers = parser.add_subparsers(help='commands', dest='command')
+    parser_run = subparsers.add_parser(
+        'run',
+        description="Run tasks",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        parents=[parser_base]
+    )
+    parser_run.add_argument('--endpoint', '-H', required=True,
+                        default='http://127.0.0.1:8000/v1/completions',
+                        help='OpenAI API-compatible endpoint')
+    parser_run.add_argument('--model', '-m', required=True,
+                        help="name of the model under test")
+    parser_run.add_argument('--tasks', '-t', required=True,
+                        help="comma separated list of tasks")
+    parser_run.add_argument('--datasets', '-d', type=dir_path, required=True,
+                        default=f"{work_dir}/datasets",
+                        help="path to dataset storage")
+    parser_run.add_argument('--batch_size', '-b', default=64, type=int,
+                        help="per-request batch size")
+    parser_run.add_argument('--output', '-o', type=argparse.FileType('w'),
+                        default=f"{work_dir}/output.json",
+                        help="results output file")
+
+    parser_list = subparsers.add_parser(
+        'list',
+        description="List available tasks",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        parents=[parser_base]
+    )
+
+    return parser
+
+
+def eval_cli():
+    local_dir = os.path.dirname(__file__)
+    work_dir = os.getcwd()
+
+    parser = setup_parser(local_dir, work_dir)
     args = parser.parse_args()
 
     logging.basicConfig(
@@ -109,6 +143,7 @@ def eval_cli():
         datefmt="%Y-%m-%d:%H:%M:%S",
         level=args.loglevel
     )
+    logger.info("CLI called with " + str(vars(args)))
 
     # Setup environment
 
@@ -125,29 +160,29 @@ def eval_cli():
     ##
     os.environ["UNITXT_ARTIFACTORIES"] = args.catalog_path
 
-    args.tasks = args.tasks.split(',')
+    if args.command == 'list':
+        list_tasks(args.tasks_path)
+    elif args.command == 'run':
+        # HACK: Working from a temporary directory allows us to load hf datasets
+        # from disk because the dataset and evaluate libraries search the local
+        # path first. Since Unitxt is loaded as a dataset, we also provide wrappers
+        # that point to the local python package.
+        with tempfile.TemporaryDirectory() as tmpdir:
+            os.chdir(tmpdir)
+            logger.info(f"Changing working directory to {tmpdir}")
 
-    logger.info("CLI called with " + str(vars(args)))
+            # Symlink unitxt wrappers to working directory
+            os.symlink(f"{local_dir}/unitxt", f"{tmpdir}/unitxt")
 
-    # HACK: Working from a temporary directory allows us to load hf datasets
-    # from disk because the dataset and evaluate libraries search the local
-    # path first. Since Unitxt is loaded as a dataset, we also provide wrappers
-    # that point to the local python package.
-    with tempfile.TemporaryDirectory() as tmpdir:
-        os.chdir(tmpdir)
-        logger.info(f"Changing working directory to {tmpdir}")
+            # Symlink datasets to working directory
+            for dataset in os.listdir(args.datasets):
+                if dataset == 'unitxt':
+                    continue # TODO Handle this better
+                os.symlink(f"{args.datasets}/{dataset}", f"{tmpdir}/{dataset}")
 
-        # Symlink unitxt wrappers to working directory
-        os.symlink(f"{local_dir}/unitxt", f"{tmpdir}/unitxt")
-
-        # Symlink datasets to working directory
-        for dataset in os.listdir(args.datasets):
-            if dataset == 'unitxt':
-                continue # TODO Handle this better
-            os.symlink(f"{args.datasets}/{dataset}", f"{tmpdir}/{dataset}")
-
-        # Call wrapped lm-eval
-        exec_lm_eval(**vars(args))
+            # Call wrapped lm-eval
+            args.tasks = args.tasks.split(',')
+            exec_lm_eval(**vars(args))
 
 
 if __name__ == '__main__':
