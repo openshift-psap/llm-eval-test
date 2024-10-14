d4436b12f891d31978629b7b30644277aec6a21a
Samuel Monson <smonson@redhat.com>
2024-10-14 15:12:45 -0400

    Move wrapped lm_eval methods to seperate file

--- perf_llm_eval/eval_bench.py
+++ perf_llm_eval/eval_bench.py
@@ -4,68 +4,11 @@ import os
 import argparse
 import logging
 import tempfile
-import json
 import dotenv
 
 logger = logging.getLogger("perf-llm-eval")
 
 dotenv.load_dotenv()
-
-def exec_lm_eval(tasks, model, endpoint, **kwargs):
-
-    # Avoid importing these until we want to exec
-    from lm_eval.evaluator import simple_evaluate
-    from lm_eval.tasks import TaskManager  # type: ignore
-    from lm_eval.utils import make_table
-
-    model_args = dict(
-        model = model,
-        base_url = endpoint,
-        num_concurent=1,
-        max_retries=3,
-        tokenizer_backend=None,
-        tokenized_requests=False
-    )
-
-    model_args_str = ','.join([f"{k}={str(v)}" for k,v in model_args.items()])
-    tm = TaskManager(
-        include_path=kwargs['tasks_path'],
-        include_defaults=False,
-        verbosity=logging.getLevelName(logger.level)
-    )
-
-    logger.info("Running lm-eval")
-    results = simple_evaluate(
-        model="local-completions",
-        model_args=model_args_str,
-        tasks=tasks,
-        #num_fewshot=self.few_shots,
-        batch_size=kwargs['batch_size'],
-        task_manager=tm,
-        verbosity=logging.getLevelName(kwargs['loglevel'])
-    )
-
-    if results:
-        # Write results to outfile
-        logger.info(f"Writing results to {kwargs['output'].name}")
-        output = json.dumps(results, indent=2, ensure_ascii=False)
-        kwargs['output'].write(output)
-
-        # Print output table
-        print(make_table(results))
-        if results.get('groups'):
-            print(make_table(results, "groups"))
-
-
-def list_tasks(tasks_path: str):
-    from lm_eval.tasks import TaskManager  # type: ignore
-    tm = TaskManager(
-        include_path=tasks_path,
-        include_defaults=False,
-        verbosity=logging.getLevelName(logger.level)
-    )
-
-    print(tm.list_all_tasks())
 
 
 def setup_parser(local_dir: str, work_dir: str):
@@ -146,22 +89,21 @@ def eval_cli():
     logger.info("CLI called with " + str(vars(args)))
 
     # Setup environment
-
     ## Disable downloads from hf
     os.environ["HF_HUB_OFFLINE"] = "1"
-
     ## Unitxt need to set this to run certain benchmarks
     os.environ["UNITXT_ALLOW_UNVERIFIED_CODE"] = "True"
-
     ## If we don't set this then Unitxt
     ## will redownload the catalog each run
     os.environ["UNITXT_USE_ONLY_LOCAL_CATALOGS"] = "True"
-
-    ##
+    ## Use local Unitxt catalog
     os.environ["UNITXT_ARTIFACTORIES"] = args.catalog_path
 
+    # Late import to avoid slow cli
+    from perf_llm_eval.lm_eval_wrapper import LMEvalWrapper
+
     if args.command == 'list':
-        list_tasks(args.tasks_path)
+        LMEvalWrapper.list_tasks(args.tasks_path)
     elif args.command == 'run':
         # HACK: Working from a temporary directory allows us to load hf datasets
         # from disk because the dataset and evaluate libraries search the local
@@ -182,7 +124,7 @@ def eval_cli():
 
             # Call wrapped lm-eval
             args.tasks = args.tasks.split(',')
-            exec_lm_eval(**vars(args))
+            LMEvalWrapper.exec(**vars(args))
 
 
 if __name__ == '__main__':
